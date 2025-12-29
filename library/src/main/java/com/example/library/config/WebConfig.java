b7fc29e21854e1698e6c4b3f84bd989550d3745c
Instreaman <instreaman@gmail.com>
2025-12-30 00:22:21 +0800

    更新 Spring Boot 版本至 3.4.1，并在 WebConfig 中添加 @NonNull 注解

--- library/src/main/java/com/example/library/config/WebConfig.java
+++ library/src/main/java/com/example/library/config/WebConfig.java
@@ -14,6 +14,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @NonNull
     @Autowired
     private LoginInterceptor loginInterceptor;
 
